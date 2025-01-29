3915b541fff5fbff8f6a42b5955fa4993ca7519b
samuelealbani <115192228+samuelealbani@users.noreply.github.com>
2025-01-29 17:00:58 +0000

    create recap version

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
+let x = 0;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220, 100, 150);
+  // background(220, 100, 150);
   fill(0,0, 255);
-  circle(150, 100, 50);
+  circle(x, 100, 30);
+  x=x+1;
+
+  noFill();
+  circle(mouseX, mouseY, 50);
 }
